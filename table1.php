603946e196f2e661d9c40de9e26bb30d05b48371
phuwadoleiei <89794730+phuwadoleiei@users.noreply.github.com>
2022-01-10 21:05:09 +0700

    Create table1.php

---
+++ table1.php
@@ -0,0 +1,20 @@
+<html>
+  <head></head>
+  <body>
+    <table border="1">
+      <?php
+        $row = 100;
+        for($i=0;$i<$row;$i++){
+          echo "<tr>";
+          echo "<td>column 1</td>";
+          echo "<td>column 2</td>";
+          echo "<td>column 3</td>";
+          echo "<td>column 4</td>";
+          echo "<td>column 5</td>";
+          echo "<td>column 6</td>";
+          echo "</tr>";
+        }
+      ?>
+    </table>
+  </body>
+</html>
