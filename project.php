03a51505778113b16ff101ea3b5c1c8320cc3d16
phuwadoleiei <89794730+phuwadoleiei@users.noreply.github.com>
2022-02-21 17:06:13 +0700

    Update project.php

--- project.php
+++ project.php
@@ -51,7 +51,7 @@
       <div class="card mb-3" style="max-width: 540px;">
         <div class="row g-0">
           <div class="col-md-4">
-            <img src="profile.jpg" class="img-fluid rounded-start" alt="...">
+            <img src="116878838_762831931132733_5383408700969080167_n';;,';,[pk,.jpg" class="img-fluid rounded-start" alt="...">
           </div>
           <div class="col-md-8">
             <div class="card-body">
